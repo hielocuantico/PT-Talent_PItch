8fad16660295422660932cc75fcf9acf35212ff8
julian.castaño_a <juliancaristizabal@gmail.com>
2024-03-20 08:22:58 -0500

    add routes delete, put, get for UserController

--- api-talentpitch/routes/api.php
+++ api-talentpitch/routes/api.php
@@ -15,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('users/search/{userName}', [UsersController::class, 'show']);
 Route::get('users', [UsersController::class, 'index']);
 Route::post('users', [UsersController::class, 'store']);
+Route::put('users/{users}', [UsersController::class, 'update']);
+Route::delete('users/{users}', [UsersController::class, 'destroy']);
