43c6be7202dbb6a20da62cb05362c65b824ae94e
julian.castaño_a <juliancaristizabal@gmail.com>
2024-03-20 08:24:31 -0500

    add complete UserController

--- api-talentpitch/app/Http/Controllers/UsersController.php
+++ api-talentpitch/app/Http/Controllers/UsersController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Http\Resources\UsersResource;
 use App\Models\Users;
-use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
@@ -13,35 +13,46 @@ class UsersController extends Controller
      */
     public function index()
     {
-        $users = Users::all();
+        $users = Users::orderBy('id','asc')->paginate(10);
+
         return UsersResource::collection( $users );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        $data = $request->validate( Users::VALIDATIONS );
+        $data = $request->validated();
         $user = Users::create( $data );
         $user->refresh();
+
         return UsersResource::make( $user );
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Users $users)
+    public function show($userName)
     {
-        //
+        $usersByName = Users::where('name', 'like', "%{$userName}%")->get();
+
+        if($usersByName->isEmpty()) {
+            return response()->json(['error' => 'No se encontraron usuarios con ese nombre.'], 404);
+        }
+
+        return UsersResource::collection( $usersByName );
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Users $users)
+    public function update(UserRequest $request, Users $users)
     {
-        //
+        $data = $request->validated();
+        $users->update($data);
+
+        return UsersResource::make($users);
     }
 
     /**
@@ -49,6 +60,7 @@ class UsersController extends Controller
      */
     public function destroy(Users $users)
     {
-        //
+        $users->delete();
+        return response()->json(['message' => _('Usuario eliminado.')], 200);
     }
 }
