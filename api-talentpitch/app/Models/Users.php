72bacf0e8ea204dc36b048c28ba58a642d9b5520
julian.castaño_a <juliancaristizabal@gmail.com>
2024-03-20 03:24:58 -0500

    fix model Users

--- api-talentpitch/app/Models/Users.php
+++ api-talentpitch/app/Models/Users.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @property int    $id
+ * @property string $name
+ * @property string $email
+ * @property string $image_path 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @property \Carbon\Carbon $deleted_at 
+ */
 
 class Users extends Model
 {
-    use HasFactory;
-}
+    use HasFactory, SoftDeletes;
+
+    public $fillable = ['name', 'email', 'image_patch'];
+
+    public const VALIDATIONS = 
+    [
+        'name'          =>  'max:255', 
+        'email'         =>  'required|max:255', 
+        'image_patch'   =>  'nullable|max:255', 
+    ];
+};
